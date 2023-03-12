8b62e5ae3c783537cea3ad15426e21258690362b
PaoloZampa <nlmp.paolozampa@gmail.com>
2023-03-12 21:16:12 +0100

    add loop js

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,9 +3,9 @@ Scrivi un programma che stampi in console i numeri da 1 a 100 ma che:
 per i multipli di 3 stampi “Fizz” al posto del numero
 per i multipli di 5 stampi “Buzz”
 per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
-/
+*/
 
-/  STRUMENTI
+/*  STRUMENTI
 - let const
 - for
 - cycle
@@ -13,8 +13,17 @@ per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
 - console.log   */
 
 //seleziono il contianer
-const divElement = document.getElementById('container'); 
+const divElement = document.getElementById('container');
 console.log(divElement);
 
-//
-
+/*
+Scrivi un programma che stampi in console i numeri da 1 a 100 ma che:
+per i multipli di 3 stampi “Fizz” al posto del numero
+per i multipli di 5 stampi “Buzz”
+per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
+*/
+for (let i = 1; i <= 100; i++){
+    const square = document.createElement("div");
+    square.classList.add("square", "d-flex", "align-items-center", "justify-content-center", "m-3");
+ 
+}
